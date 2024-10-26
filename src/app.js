906fd266f04a533bf9b9f3d1b2fa66db2fd2aa89
enlian <frejaenlian@gmail.com>
2024-10-26 15:15:28 +0900

    fix bug

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Root from './router';
+import logo from './logo.svg';
+import './App.css';
 
-//globe css
-import './style/index.styl';
-import './style/less.less';
-import './style/sass.sass';
-import './style/scss.scss';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
-ReactDOM.render(<Root />, document.getElementById('app'));
+export default App;
--- src/redux/reduces/index.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable import/no-named-as-default */
-import {combineReducers} from 'redux';
-import home from './home';
-
-export default combineReducers({
-  home
-});
